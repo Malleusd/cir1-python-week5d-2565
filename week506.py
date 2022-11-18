008b7bb6797858f03dbd5b1c5affa38a8f28cd0d
Malleusd <malleusdraconia63@gmail.com>
2022-11-18 11:25:56 +0700

    fon

---
+++ week506.py
@@ -0,0 +1,9 @@
+# num = 4
+num = float(input("Enter number"))
+if num >= 0:
+    if num == 0:
+        print("Even")
+    else:
+        print("positive")
+else:
+    print(num, "is Negative number")
